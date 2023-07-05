595c9937e5a3d0643f4edde772a3305a05ffba65
Reinharth Garces <rjgarces7@gmail.com>
2023-07-04 23:27:21 -0300

    add de routers y metodo post

--- src/app.js
+++ src/app.js
@@ -1,82 +1,35 @@
 const express = require ('express')
-const ProductManager = require ('./ProductManager')
-const manager = new ProductManager('./products.json')
+const productsRouter = require('./routes/products')
+// const cartsRouter = require('./routes/carts')
+
+
 
 const app = express()
 const server = app.listen(8080, () =>console.log('Servidor arriba desde puerto 8080'))
 
-
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
-// app.use('/api/users/', usersRouter)
-// app.use('/api/pets/', petsRouter)
-
-
-
-app.get('/products', async (req, res) => {
-  try {
-    const limit = parseInt(req.query.limit)
-    const products = await manager.getProducts() 
-
-    if (!isNaN(limit) && limit > 0) {
-      const limitedProducts = products.slice(0, parseInt(limit))
-      console.log(limitedProducts)
-      return res.json(limitedProducts)
-    }
-
-    res.json(products) 
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Error al obtener los productos' })
-  }
-})
-
-app.get('/products/:pid', async (req, res) => {
-  try {
-    const productId = parseInt(req.params.pid)
-
-    const product = await manager.getProductsById(productId)
-
-    if (!product) {
-      return res.status(404).json({ error: 'Producto no encontrado' })
-    }
-
-    res.json(product)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Error al obtener el producto' })
-  }
+app.use('/static', express.static('public'))
+app.use('/api/products', productsRouter)
+// app.use('/api/carts', cartsRouter)
+app.use((req, res, next) => {
+  console.log('Middleare a nivel aplicación')
+  return next()
 })
 
 
-//Metodo POST
-let users = []
-
-app.post('/api/user', (req, res) => {
-  let user = req.body
-  
-  if(!user.firstName || !user.lastName) {
-    return res.status(400).send ({status: "error", error: "Incomplete values"})
-  }
-
-  users.push(user)
-  res.send({status: "succes", message: "User Created"})
-  console.log(user)
-})
 
 //Metodo DELETE
-
-app.delete('/api/user/:name', (req,res) =>{
-  let name = req.params.name
-  let currentLength = users.length
-  console.log(users)
-  users = users.filter( user=>user.firstName!=name )
-  if(users.lenght===currentLength){
-    return res.status(404).send({status:"error", error: "User not found"})
-  }
-  res.send({status:"success", message: "User deleted"})
-})
+// app.delete('/api/user/:name', (req,res) =>{
+//   let name = req.params.name
+//   let currentLength = users.length
+//   console.log(users)
+//   users = users.filter( user=>user.firstName!=name )
+//   if(users.lenght===currentLength){
+//     return res.status(404).send({status:"error", error: "User not found"})
+//   }
+//   res.send({status:"success", message: "User deleted"})
+// })
 
 
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,8 @@
 const { Router } = require('express')
-const uploader = require('../utils/utils')
+
 
 const productsRouter = Router()
-const products = []
+let products = []
 
 
 productsRouter.use((req,res,next) =>{
@@ -10,13 +10,54 @@ productsRouter.use((req,res,next) =>{
   return next()
 })
 
-productsRouter.get('/', async (req, res) => {
-  
-})
+// productsRouter.get('/api/products', async (req, res) => {
+//   try {
+//     const limit = parseInt(req.query.limit)
+//     const products = await manager.getProducts() 
 
+//     if (!isNaN(limit) && limit > 0) {
+//       const limitedProducts = products.slice(0, parseInt(limit))
+//       console.log(limitedProducts)
+//       return res.json(limitedProducts)
+//     }
 
-// router.get('/', (req,res) => {
-  
+//     res.json(products) 
+//   } catch (error) {
+//     console.error(error)
+//     res.status(500).json({ error: 'Error al obtener los productos' })
+//   }
 // })
 
-// export default router;
+// productsRouter.get('/api/products/:pid', async (req, res) => {
+//   try {
+//     const productId = parseInt(req.params.pid)
+
+//     const product = await manager.getProductsById(productId)
+
+//     if (!product) {
+//       return res.status(404).json({ error: 'Producto no encontrado' })
+//     }
+
+//     res.json(product)
+//   } catch (error) {
+//     console.error(error)
+//     res.status(500).json({ error: 'Error al obtener el producto' })
+//   }
+// })
+
+
+//Metodo POST
+productsRouter.post('/', (req, res) => {
+  const product = req.body
+
+  if(!product || Object.values(product).some(value => !value)){
+    return res.status(400).send({status: "error", error: "Incomplete values"})
+  }
+
+  product.id = products.length + 1
+  products.push(product)
+  console.log(products)
+  return res.status(201).json({status: "success",  message:"Product created" })
+})
+
+module.exports = productsRouter
